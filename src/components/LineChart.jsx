98532518e7fc0324f738aa3cdf0d7838642e024e
Gabe <gabenedden@gmail.com>
2022-01-25 10:42:34 -0500

    Add Linechart to cryptodetails page

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,10 +1,47 @@
 import React from 'react';
-
+import { Line }from 'react-chartjs-2';
 import { Col, Row, Typography } from 'antd';
 
 const { Title } = Typography;
 
 const LineChart = ({coinHistory, currentPrice, coinName}) => {
+    const coinPrice = [];
+    const coinTimestamp = [];
+
+    for(let ii = 0; ii < coinHistory?.data?.history?.length; ii++){
+        coinPrice.push(coinHistory.data.history[ii].price);
+        coinTimestamp.push(new Date(coinHistory.data.history[ii].timestamp).toLocaleDateString());
+    }
+
+    const data = {
+        labels: coinTimestamp,
+        datasets: [
+            {
+                label: 'Price in USD',
+                data: coinPrice,
+                fill: false,
+                backgroundColor: '#0071bd',
+                borderColor: '#0071bd'
+            }
+        ]
+    }
+
+    const options = {
+        scales: {
+            yAxes: [
+                {
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }
+            ]
+        }
+    }
+
+    console.log(data)
+    console.log(options)
+
+
   return (
     <>
         <Row className='chart-header'>
@@ -14,6 +51,7 @@ const LineChart = ({coinHistory, currentPrice, coinName}) => {
                 <Title level={5} className='current-change'>Current {coinName} Price: $ {currentPrice}</Title>
             </Col>
         </Row>
+        <Line data={data} options={options} />
     </>
   )
 };
