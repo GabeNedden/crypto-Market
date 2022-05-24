e6f3fbacca76594dbdbbd10292b8846640a3e771
Gabe <gabenedden@gmail.com>
2022-05-24 10:19:39 -0400

    Testing commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -38,7 +38,7 @@ const Navbar = () => {
       <div className="nav-container">
           <div className="logo-container">
             <Avatar src={icon} size="large" />
-            <Typography.Title level={2} className="logo"><Link to="/">Crypto Market</Link></Typography.Title>
+            <Typography.Title level={2} className="logo"><Link to="/">Crypto Market!</Link></Typography.Title>
             <Button className="menu-control-container" onClick={() => setActiveMenu(!activeMenu)}><MenuOutlined /></Button>
           </div>
           {activeMenu && (
